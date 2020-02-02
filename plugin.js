90433727c8f4ba0363e94fc2cd17539ee8ce903a
watduhhekbro <watduhhekbro@gmail.com>
2020-02-02 01:10:35 -0600

    No longer breaks icons and variables, made it so the status node could work with other mods, fixed the "oo" in "Rookie Harbor", and added more cases

--- plugin.js
+++ plugin.js
@@ -8,18 +8,31 @@ export default class uwuifier extends Plugin
 		this.RELATIVE_DIR = this.BASE_DIR.substring(7);
 		this.LEXICON = {
 			'them': 'dem',
+			'then': 'den',
+			'they': 'dey',
+			'there': 'der',
+			'these': 'deez',
+			'give': 'giev',
 			'the': 'da',
+			'and': 'an',
 			'th': 'd',
+			'wh': 'w',
+			'rh': 'w',
+			'gh': '',
 			'l': 'w',
 			'r': 'w',
-			'oo': '\u0000',
 			'o': 'owo',
 			'u': 'uwu',
-			'\u0000': 'uwu',
-			'\\.': '\u0000',
 			'.': ' uwu.',
-			'\u0000': '\\.',
 			'?': ' owo?'
+		};
+		this.APPREHEND = {
+			'just': 'jus',
+			'you': 'yuwu',
+			'oo': 'uwu',
+			'ou': 'ouwu',
+			'\\.': '\\.',
+			'or': 'ow'
 		};
 	}
 	
@@ -33,10 +46,16 @@ export default class uwuifier extends Plugin
 		ig.LangLabel.inject({
 			init: function(a)
 			{
-				if(a && a.status !== 'uwuified')
+				if(a)
 				{
-					a.en_US = self.generateDumpsterFire(a.en_US.toLowerCase());
-					a.status = 'uwuified';
+					if(!a.status)
+						a.status = '';
+					
+					if(!a.status.includes('uwuified'))
+					{
+						a.en_US = self.generateDumpsterFire(a.en_US.toLowerCase());
+						a.status += 'uwuified';
+					}
 				}
 				this.parent(...arguments);
 			}
@@ -46,8 +65,47 @@ export default class uwuifier extends Plugin
 	generateDumpsterFire(line)
 	{
 		if(line && line.constructor === String)
+		{
+			// Press \\i[key-up]\\i[key-left]\\i[key-down]\\i[key-right] to move around
+			// You are at \\c[3]level \\v[player.level]\\c[0]!
+			let stack = [];
+			let selection = '';
+			let mode = false;
+			
+			for(let i = 0; i < line.length; i++)
+			{
+				if(line.substring(i-3, i) === '\\v[' || line.substring(i-3, i) === '\\i[')
+					mode = true;
+				else if(mode && line[i] === ']')
+				{
+					mode = false;
+					stack.push(selection);
+					line = line.replace(selection, '\u0000');
+					i -= selection.length - 1;
+					selection = '';
+				}
+				
+				if(mode)
+					selection += line[i];
+			}
+			
+			let index = 0;
+			
+			for(let key in this.APPREHEND)
+				line = line.split(key).join('\u0001' + index++);
+			
 			for(let key in this.LEXICON)
 				line = line.split(key).join(this.LEXICON[key]);
+			
+			index = 0;
+			
+			for(let key in this.APPREHEND)
+				line = line.split('\u0001' + index++).join(this.APPREHEND[key]);
+			
+			while(stack.length > 0) {
+				line = line.replace('\u0000', stack.shift());
+			}
+		}
 		return line;
 	}
 }
